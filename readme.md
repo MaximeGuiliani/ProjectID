dc55567d9e58d7230ca9913d247121a36de1dda7
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-02-02 17:44:10 +0100

    readme

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 Scénario :
+
+TODO : 
+
+- S'occuper du médiateur et des requetes utilisateurs comment recuperer depuis le wrapper
+- Appeler les requêtes pour la base de données 
+
+
+- vues
+- ~~wrapper~~
+- exemple de requêtes
+- Si Java : un jar
--- src/main/java/com/Main/Main.java
+++ src/main/java/com/Main/Main.java
@@ -2,7 +2,10 @@ package com.Main;
 
 public class Main {
     public static void main(String[] args) {
-        WrapperCSV wcsv = new WrapperCSV();       
+        WrapperCSV wcsv = new WrapperCSV("dbName","tableName","/Users/max/Desktop/ID/src/main/java/com/WrapperCSV/marseille-stations-taxis-2017.csv", ',',false );
+        System.out.println( wcsv.convertToSQL());
     }
+
+
     
 }
